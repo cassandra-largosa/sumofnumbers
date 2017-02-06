340cd3718e76e1b53c3d7d115308242a5f221448
Cassandra Largosa <cjlargos@hawaii.edu>
2017-02-05 17:00:16 -1000

    Third times the charm

--- sumofnumbers/sumofnumbers.js
+++ sumofnumbers/sumofnumbers.js
@@ -1,43 +1,42 @@
 /**
  * solving summing numbers with in different ways
- * Created by Adama on 2/5/2017.
+ * Created by Cassandra Largosa on 2/5/2017.
  */
 
 const testList = [1, 2, 3];
 
 function sumFor(nums) {
   let sum = 0;
-  for (let num of nums) {
+  for (num of nums) {
     sum += num;
   }
   return sum;
 }
 
-console.log(`for-loop: ${sumFor(testList)}`);
+console.log(`for: ${sumFor(testList)}`);
 
 function sumWhile(nums) {
   let sum = 0;
   let i = 0;
   while (i < nums.length) {
     sum += nums[i];
-    i++;
   }
   return sum;
 }
 
-console.log(`while-loop: ${sumWhile(testList)}`);
+console.log(`while: ${sumWhile(testList)}`);
 
 function sumRecursive(nums) {
   if (nums.length === 0) {
     return 0;
   }
-  return nums[0] + sumRecursive(nums.slice(1,nums.length));
+  return nums.shift() + sumRecursive(nums);
 }
 
-console.log(`recursion: ${sumRecursive(testList, 0, testList.length)}`);
+console.log(`recursive: ${sumRecursive(testList)}`);
 
 function sumTheSimpleWay(nums) {
-  return _.reduce(nums, function(mem, num) {return mem + num;});
+  return _.redeuce(nums, function(mem, num) { return mem + num });
 }
 
-console.log(`underscore: ${sumTheSimpleWay(testList)}`);
+console.log(`underescore ${sumTheSimpleWay(testList)}`);
